066f98b3e7341ac964472279420829a445a1409b
Fábio Lentino <fabio_lentino@hotmail.com>
2025-10-07 19:44:13 -0300

    Create enviar.php

---
+++ enviar.php
@@ -0,0 +1,26 @@
+<?php
+// Define o endereço para onde a mensagem será enviada
+$to = "develentino@gmail.com"; 
+$subject = "Nova Mensagem do Portfólio de Fábio Lentino";
+
+// Coleta os dados do formulário
+$name = htmlspecialchars($_POST['Nome']);
+$email = htmlspecialchars($_POST['_replyto']);
+$message = htmlspecialchars($_POST['Mensagem']);
+
+$headers = "From: " . $email . "\r\n";
+$headers .= "Reply-To: " . $email . "\r\n";
+$email_body = "Nome: $name\n";
+$email_body .= "E-mail: $email\n\n";
+$email_body .= "Mensagem:\n$message";
+
+// Envia o e-mail
+if (mail($to, $subject, $email_body, $headers)) {
+    // Redireciona o usuário para uma página de sucesso
+    header('Location: obrigado.html');
+    exit;
+} else {
+    // Caso de falha
+    echo "Erro ao enviar a mensagem.";
+}
+?>
